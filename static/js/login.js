b5b6eeb999083807801b65f16258cd08379397f1
daH005 <danil.shevelev.2004@mail.ru>
2023-11-06 18:39:45 +0500

    Сделал авторизацию с использованием API.

---
+++ static/js/login.js
@@ -0,0 +1,27 @@
+import {HTTP_AUTH_URL, AUTH_COOKIE_MAX_AGE, AUTH_TOKEN_COOKIE_KEY} from "./config.js";
+import {setCookie} from "./cookie.js";
+
+const usernameInputEl = document.getElementById("js-username-input");
+const passwordInputEl = document.getElementById("js-password-input");
+const buttonEl = document.getElementById("js-button");
+
+buttonEl.onclick = () => {
+    auth(usernameInputEl.value, passwordInputEl.value);
+}
+
+async function auth(username, password) {
+    let response = await fetch(HTTP_AUTH_URL, {
+        method: "POST",
+        body: JSON.stringify({username, password}),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (response.ok) {
+        let data = await response.json();
+        setCookie(AUTH_TOKEN_COOKIE_KEY, data.authToken, AUTH_COOKIE_MAX_AGE);
+        window.location.href = '/';
+    } else {
+        alert("Неверный логин или пароль!");
+    }
+}
