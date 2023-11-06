c86631f04a0e5882cd9435650761dbef47d96ed4
daH005 <danil.shevelev.2004@mail.ru>
2023-11-06 18:39:16 +0500

    Создал модуль с конфигом (адреса серверов и др. настройки).

---
+++ static/js/config.js
@@ -0,0 +1,9 @@
+export const WEBSOCKET_BASE_URL = "ws://localhost:80";
+
+export const HTTP_BASE_URL = "http://localhost:81";
+export const HTTP_AUTH_URL = HTTP_BASE_URL + "/auth";
+export const HTTP_USER_CHATS_URL = HTTP_BASE_URL + "/userChats";
+export const HTTP_CHAT_HISTORY = HTTP_BASE_URL + "/chatHistory";
+
+export const AUTH_COOKIE_MAX_AGE = 3600 * 24 * 365;
+export const AUTH_TOKEN_COOKIE_KEY = "authToken";
