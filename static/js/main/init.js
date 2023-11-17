0a1b83d4606b6bf4073caf0657fc85d082a97b1e
daH005 <danil.shevelev.2004@mail.ru>
2023-11-17 22:09:10 +0500

    Оставил FixMe-комментарий.

--- static/js/main/init.js
+++ static/js/main/init.js
@@ -3,6 +3,7 @@ import { requestUserInfo, requestUserChats, redirectToLoginPage } from "./_http.
 import { startWebSocket } from "./_websocket.js";
 import { displayUserInfo, displayUserChats, displayChatMessage } from "./_html.js";
 
+// FixMe: Ещё важнее: после запуска nginx код перестал работать, как задумывалось.
 // Важно: запрещает переход вперёд-назад по истории браузера (а точнее вкладки).
 // Работает, как на ПК, так и на телефоне.
 window.addEventListener("popstate", () => {
