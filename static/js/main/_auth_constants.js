b90f8326f968eaeaff5922f704bdf6481319a5ae
daH005 <danil.shevelev.2004@mail.ru>
2023-11-25 15:46:37 +0500

    Переделал модуль. Создал функцию для формирования заголовков с токеном.

--- static/js/main/_auth_constants.js
+++
@@ -1,9 +0,0 @@
-import { JWT_TOKEN_LOCAL_STORAGE_KEY, BASE_HEADERS } from "../_config.js";
-
-// JWT-токен, необходимый для работы с нашим API (HTTP + WebSocket).
-export const JWT_TOKEN = localStorage.getItem(JWT_TOKEN_LOCAL_STORAGE_KEY);
-// Переопределяет базовые заголовки, добавив в них JWT-токен.
-export const BASE_AUTH_HEADERS = {
-    ...BASE_HEADERS,
-    Authorization: `Bearer ${JWT_TOKEN}`,
-}
---
+++ static/js/main/_auth_tools.js
@@ -0,0 +1,10 @@
+import { BASE_HEADERS } from "../_config.js";
+import { getJWTToken } from "../_local_storage.js";
+
+// Формирует заголовки запроса с 'Content-Type' + JWT-токеном.
+export function makeAuthHeaders() {
+    return {
+        ...BASE_HEADERS,
+        Authorization: `Bearer ${getJWTToken()}`,
+    }
+}
