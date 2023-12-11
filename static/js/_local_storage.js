a1dca9e75ae78fd6f03b3e633ac033d9c9ffd183
daH005 <danil.shevelev.2004@mail.ru>
2023-12-11 17:42:32 +0500

    Написал комментарии к коду.

--- static/js/_local_storage.js
+++ static/js/_local_storage.js
@@ -1,9 +1,12 @@
 import { JWT_TOKEN_LOCAL_STORAGE_KEY } from "./_config.js";
 
+// Сохраняет JWT-токен в `localStorage`.
 export function saveJWTToken(token) {
     localStorage.setItem(JWT_TOKEN_LOCAL_STORAGE_KEY, token);
 }
 
+// Выдаёт JWT-токен из `localStorage`. Если токена там нет, то возвращается
+// "undefined" / "null" (как показала практика, это именно строки).
 export function getJWTToken() {
     return localStorage.getItem(JWT_TOKEN_LOCAL_STORAGE_KEY);
 }
