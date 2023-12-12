7324cf48b9fecadde22dbbdea5f4fa23a7b7fec6
daH005 <danil.shevelev.2004@mail.ru>
2023-12-12 13:57:15 +0500

    Сделал переключение показа / скрытия пароля на странице авторизации.

--- static/js/login.js
+++ static/js/login.js
@@ -5,6 +5,8 @@ import { saveJWTTokenAndRedirect } from "./_auth_tools.js";
 const usernameInputEl = document.getElementById("js-username");
 const passwordInputEl = document.getElementById("js-password");
 const buttonEl = document.getElementById("js-button");
+const passwordSwitchButtonEl = document.getElementById("js-password-switch");
+const eyeEl = passwordSwitchButtonEl.querySelector("i");
 
 buttonEl.onclick = async () => {
     let username = usernameInputEl.value;
@@ -16,5 +18,15 @@ buttonEl.onclick = async () => {
         saveJWTTokenAndRedirect(data.JWTToken);
     } else {
         alert("Логин или пароль пусты!...");
+    }
+}
+
+// Показ и сокрытие пароля.
+passwordSwitchButtonEl.onclick = () => {
+    passwordInputEl.type = passwordInputEl.type == "password" ? "text" : "password";
+    if (passwordInputEl.type == "password") {
+        eyeEl.className = eyeEl.className.replace("fa-eye", "fa-eye-slash");
+    } else {
+        eyeEl.className = eyeEl.className.replace("fa-eye-slash", "fa-eye");
     }
 }
