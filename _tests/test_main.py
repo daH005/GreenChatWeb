5fd7b652f1b30bde5e658d84e9d4061873910d03
danil <danil.shevelev.2004@mail.ru>
2024-01-22 12:28:39 +0500

    Удалил уже неактуальный файл

--- _tests/test_main.py
+++
@@ -1,6 +0,0 @@
-from web._tests.common import driver, FullUrl  # noqa
-
-
-def test_positive_main_page_redirects_to_login_if_user_is_not_auth() -> None:
-    driver.get(FullUrl.MAIN)
-    assert driver.current_url == FullUrl.LOGIN
