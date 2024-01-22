537174d1a98f2d2f1ea282eb5e957a5245370e1c
danil <danil.shevelev.2004@mail.ru>
2024-01-22 11:54:26 +0500

    Разбил модуль `common` на пакет

---
+++ _tests/common/constants.py
@@ -0,0 +1,19 @@
+from typing import Final
+from enum import StrEnum
+
+from web.config import HOST, PORT
+from web.endpoints import Url
+
+__all__ = (
+    'WEBSITE_URL',
+    'FullUrl',
+)
+
+host: str = HOST.replace('0.0.0.0', 'localhost')
+WEBSITE_URL: Final[str] = f'http://{host}:{PORT}'
+
+
+class FullUrl(StrEnum):
+    MAIN = WEBSITE_URL + Url.MAIN
+    REG = WEBSITE_URL + Url.REG
+    LOGIN = WEBSITE_URL + Url.LOGIN
