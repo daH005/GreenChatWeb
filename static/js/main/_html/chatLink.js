a586bbf6464380074062d0dbf43008adc5005e65
danil <danil.shevelev.2004@mail.ru>
2024-01-29 14:47:22 +0500

    Стилизировал приставку "Вы:"

--- static/js/main/_html/chatLink.js
+++ static/js/main/_html/chatLink.js
@@ -3,7 +3,7 @@ import { AbstractChatElementFacade } from "./abs.js";
 const chatLinkParentEl = document.getElementById("js-all-chats-links");
 const chatLinkTempEl = document.getElementById("js-chat-link-temp");
 
-const MAX_CHAT_LINK_TEXT_LENGTH = 20;
+const MAX_CHAT_LINK_TEXT_LENGTH = 15;
 
 export class ChatLink extends AbstractChatElementFacade {
 
@@ -30,7 +30,8 @@ export class ChatLink extends AbstractChatElementFacade {
         this.childEls.name = this.el.querySelector(".chat-link__chat-name");
         this.childEls.name.textContent = this.data.name;
 
-        this.childEls.lastMessage = this.el.querySelector(".chat-link__last-message");
+        this.childEls.self = this.el.querySelector(".chat-link__last-message .chat-link__self");
+        this.childEls.text = this.el.querySelector(".chat-link__last-message .chat-link__text");
         this.childEls.lastMessageDate = this.el.querySelector(".chat-link__date");
     }
 
@@ -38,13 +39,15 @@ export class ChatLink extends AbstractChatElementFacade {
         let text = data.text;
 
         if (data.isSelf) {
-            text = "Вы: " + text;
+            this.childEls.self.style = "";
+        } else {
+            this.childEls.self.style = "display: none;";
         }
 
         if (text.length > MAX_CHAT_LINK_TEXT_LENGTH) {
             text = text.slice(0, MAX_CHAT_LINK_TEXT_LENGTH) + "...";
         }
-        this.childEls.lastMessage.textContent = text;
+        this.childEls.text.textContent = text;
         this.childEls.lastMessageDate.textContent = data.dateStr;
 
         this.parentEl.prepend(this.el);
