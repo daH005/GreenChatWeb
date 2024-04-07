b69344b053e06963a0ac3740802e584344cf86da
danil <danil.shevelev.2004@mail.ru>
2024-04-07 10:23:31 +0500

    Элемент `sidebar__id` удалён

--- static/js/main/_html/userInfo.js
+++ static/js/main/_html/userInfo.js
@@ -1,13 +1,5 @@
-const userIdParentEl = document.getElementById("js-user-id");
-const userIdSpanEl = userIdParentEl.querySelector("span");
 const userNameEl = document.getElementById("js-user-name");
 
-userIdParentEl.onclick = () => {
-    navigator.clipboard.writeText(userIdSpanEl.textContent);
-    notify("ID успешно скопирован!");
-}
-
 export function updateUserInfo(user) {
-    userIdSpanEl.textContent = user.id;
     userNameEl.textContent = user.firstName + " " + user.lastName;
 }
