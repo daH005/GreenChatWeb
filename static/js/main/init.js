5a5430bacbbe136a62f498a570216bdfc925ba35
danil <danil.shevelev.2004@mail.ru>
2024-04-07 12:52:06 +0500

    Обновление токена при каждой перезагрузке страницы

--- static/js/main/init.js
+++ static/js/main/init.js
@@ -4,10 +4,15 @@ import { JWT_TOKEN_REFRESH_INTERVAL_DELAY } from "../_config.js";
 import { startWebSocket } from "./_websocket.js";
 import { initHtml, handlersForWebsocket } from "./_html/init.js";
 
-setInterval(async () => {
+async function updateJWTToken() {
     let data = await requestNewJWTToken();
     JWTToken.set(data.JWTToken);
     console.log("Токен обновлён!");
+}
+
+updateJWTToken();
+setInterval(async () => {
+    updateJWTToken();
 }, JWT_TOKEN_REFRESH_INTERVAL_DELAY);
 
 await initHtml();  // important! wait all chats loading and other things
