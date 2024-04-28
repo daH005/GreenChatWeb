a7fb99b74a058b2592fb9b9edf25e959eeeddb1f
danil <danil.shevelev.2004@mail.ru>
2024-04-28 12:57:29 +0500

    Вернул отображение ID пользователя

--- static/js/main/_html/userInfo.js
+++ static/js/main/_html/userInfo.js
@@ -1,5 +1,7 @@
 const userNameEl = document.getElementById("js-user-name");
+const userIdEl = document.getElementById("js-user-id");
 
 export function updateUserInfo(user) {
     userNameEl.textContent = user.firstName + " " + user.lastName;
+    userIdEl.textContent = user.id;
 }
