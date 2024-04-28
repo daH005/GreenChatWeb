aeea9d0b6ed1520324f193e3b49a90b58796452a
danil <danil.shevelev.2004@mail.ru>
2024-04-28 12:59:29 +0500

    Вернул копирование ID при клике

--- static/js/main/_html/userInfo.js
+++ static/js/main/_html/userInfo.js
@@ -1,5 +1,11 @@
 const userNameEl = document.getElementById("js-user-name");
+const userIdParentEl = document.getElementById("js-user-id-parent");
 const userIdEl = document.getElementById("js-user-id");
+
+userIdParentEl.onclick = () => {
+    navigator.clipboard.writeText(userIdEl.textContent);
+    notify("ID успешно скопирован!");
+}
 
 export function updateUserInfo(user) {
     userNameEl.textContent = user.firstName + " " + user.lastName;
