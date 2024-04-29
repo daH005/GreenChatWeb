0eea61cda8c59a6ed71c0f4ff31e49399ddce710
danil <danil.shevelev.2004@mail.ru>
2024-04-29 22:44:26 +0500

    Refactoring of selenium-tests

--- _tests/test_all.py
+++ _tests/test_all.py
@@ -1,128 +1,181 @@
 from __future__ import annotations
 from selenium.webdriver.common.by import By
 from selenium.webdriver import Chrome
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.common.exceptions import NoSuchElementException
 from time import sleep
+from itertools import combinations
+from typing import Iterator
 
 from web._tests.common.drivers import new_chrome_driver  # noqa
-from web._tests.common.constants import FullUrl  # noqa
-from web._tests.common.info_generation import NewUserInfo  # noqa
+from web._tests.common.constants import EMAIL_CODE, FullUrl  # noqa
+from web._tests.common.info_generation import UserInfo  # noqa
 from web._tests.all_test_data import *  # noqa
 
-driver: Chrome = new_chrome_driver()
 
+class TestAll:
 
-def test_positive_all() -> None:
-    _test_positive_main_page_redirects_to_login_if_user_is_not_auth()
+    driver: Chrome = new_chrome_driver()
+    users: list[UserInfo] = []
+    users_pairs_with_histories: list[tuple[UserInfo, UserInfo, list[str]]] = []
+    iter_histories: Iterator[list[str]] = iter(HISTORIES)
 
-    # registration
-    users = []
-    for _ in range(3):
-        user = NewUserInfo.new_and_count()
-        users.append(user)
-        _test_positive_login(user)  # as registration
+    cur_user: UserInfo
+    cur_chat_el: WebElement
 
-    # check id
-    for user in users:
-        _test_positive_login(user)
-        assert user.id == _get_my_id()
+    @classmethod
+    def teardown_class(cls) -> None:
+        cls.driver.quit()
 
-    # messages sendings
-    users_pairs_data = [
-        [users[0], users[1], HISTORIES[0]],
-        [users[0], users[2], HISTORIES[1]],
-        [users[1], users[2], HISTORIES[2]],
-    ]
-    for data in users_pairs_data:
-        _test_positive_login(data[0])
-        _test_positive_create_new_chat(data[1].id, data[2][0])
-        cur_user = data[0]
+    @classmethod
+    def test_positive_all(cls) -> None:
+        cls._test_negative_redirect_to_login_if_not_auth()
 
-        for step in data[2][1:]:
+        cls._register_all()
+        cls._test_positive_redirect_to_main_after_login(cls.users[0])
 
-            # user changing
-            if step == CHANGE_MARK:
-                old_user = cur_user
-                cur_user: NewUserInfo = data[0]
-                if old_user == data[0]:
-                    cur_user = data[1]
-                _test_positive_close_cur_chat()
-                _test_positive_login(cur_user)
-                _test_positive_search(old_user.id)
-                continue
+        cls._make_users_pairs_with_histories()
+        cls._make_chats_and_check_histories()
 
-            _test_positive_send_messages_in_cur_chat([step])
+    @classmethod
+    def _test_negative_redirect_to_login_if_not_auth(cls) -> None:
+        cls.driver.get(FullUrl.MAIN)
+        sleep(0.1)
+        assert cls.driver.current_url == FullUrl.LOGIN
 
-        history_without_change_mark = list(filter(lambda x: x != CHANGE_MARK, data[2].copy()))
-        _test_positive_history(history_without_change_mark)
+    @classmethod
+    def _register_all(cls) -> None:
+        for _ in range(USERS_COUNT):
+            cls._register_new()
 
+    @classmethod
+    def _register_new(cls) -> None:
+        user: UserInfo = UserInfo.new()
+        cls.users.append(user)
+        cls._login(user)  # as registration
 
-def _get_my_id() -> int:
-    return int(driver.find_element(By.XPATH, '//span[@id="js-user-id"]').text)
+    @classmethod
+    def _login(cls, user: UserInfo) -> None:
+        cls.driver.get(FullUrl.LOGIN)
 
+        cls.driver.find_element(By.XPATH, '//input[@id="js-email"]').send_keys(user.email)
+        cls.driver.find_element(By.XPATH, '//input[@id="js-email-code"]').send_keys(EMAIL_CODE)
 
-def _test_positive_main_page_redirects_to_login_if_user_is_not_auth() -> None:
-    driver.get(FullUrl.MAIN)
-    assert driver.current_url == FullUrl.LOGIN
+        cls.driver.find_element(By.XPATH, '//button[@id="js-button"]').click()
+        sleep(0.5)  # wait api authorization
 
+        user.id = cls._get_my_id()
+        cls.cur_user = user
 
-def _test_positive_login(user_info: NewUserInfo) -> None:
-    driver.get(FullUrl.LOGIN)
+    @classmethod
+    def _get_my_id(cls) -> int:
+        return int(cls.driver.find_element(By.XPATH, '//span[@id="js-user-id"]').text)
 
-    driver.find_element(By.XPATH, '//input[@id="js-email"]').send_keys(user_info.email)
-    driver.find_element(By.XPATH, '//input[@id="js-email-code"]').send_keys(user_info.code)
+    @classmethod
+    def _test_positive_redirect_to_main_after_login(cls, user: UserInfo) -> None:
+        cls._login(user)
+        assert cls.driver.current_url == FullUrl.MAIN
 
-    driver.find_element(By.XPATH, '//button[@id="js-button"]').click()
-    sleep(0.5)  # wait api authorization
+    @classmethod
+    def _make_chats_and_check_histories(cls) -> None:
+        for first_user, second_user, history in cls.users_pairs_with_histories:
+            cls._login(first_user)
+            cls._create_new_chat(second_user.id, history[0])
 
-    assert driver.current_url == FullUrl.MAIN
+            for cur_text_message in history[1:]:
+                if cur_text_message == CHANGE_MARK:
+                    cls._change_users(first_user, second_user)
+                    continue
+                cls._send_message_in_cur_chat(cur_text_message)
 
+            history_without_change_mark = list(filter(lambda x: x != CHANGE_MARK, history.copy()))
+            cls._test_positive_history(history_without_change_mark)
 
-def _test_positive_create_new_chat(id_: int,
-                                   first_text_message: str,
-                                   ) -> None:
-    _test_positive_search(id_)
+    @classmethod
+    def _make_users_pairs_with_histories(cls) -> None:
+        users_pairs: list[tuple[UserInfo, UserInfo]] = combinations(cls.users, 2)
+        cur_history: list[str]
+        for pair in users_pairs:
+            cur_history = cls._next_history()
+            cls.users_pairs_with_histories.append(
+                (*pair, cur_history)  # type: ignore
+            )
 
-    driver.find_element(By.XPATH, '//div[@id="js-new-chat"]//textarea').send_keys(first_text_message)
-    driver.find_element(By.XPATH, '//div[@id="js-new-chat"]//button').click()
-    sleep(0.5)  # wait for api chat creating
+    @classmethod
+    def _next_history(cls) -> list[str]:
+        try:
+            return next(cls.iter_histories)
+        except StopIteration:
+            cls.iter_histories = iter(HISTORIES)
+            return next(cls.iter_histories)
 
+    @classmethod
+    def _create_new_chat(cls, interlocutor_id: int,
+                         first_text_message: str,
+                         ) -> None:
+        cls._search_interlocutor(interlocutor_id)
 
-def _test_positive_search(id_: int) -> None:
-    input_el = driver.find_element(By.XPATH, '//input[@id="js-search-input"]')
-    input_el.clear()
-    input_el.send_keys(str(id_))
-    driver.find_element(By.XPATH, '//button[@id="js-search-button"]').click()
-    sleep(0.5)  # wait for switch and history loading
+        cls.driver.find_element(By.XPATH, '//div[@id="js-new-chat"]//textarea').send_keys(first_text_message)
+        cls.driver.find_element(By.XPATH, '//div[@id="js-new-chat"]//button').click()
+        sleep(0.5)  # wait for api chat creating
 
+        cls._find_cur_chat_el()
 
-# FixMe: Вернуть прогон теста после того, как сделаем редактирование имени и фамилии
-def _test_positive_switch_to_chat_by_sidebar(interlocutor_full_name: str) -> None:
-    driver.find_element(By.XPATH, f'//div[@class="sidebar__links"]'
-                                  f'/div[@class="chat-link"]'
-                                  f'//div[@class="chat-link__chat-name"][text()="{interlocutor_full_name}"]').click()
-    sleep(0.5)  # wait for switch and history loading
+    @classmethod
+    def _search_interlocutor(cls, interlocutor_id: int) -> None:
+        input_el = cls.driver.find_element(By.XPATH, '//input[@id="js-search-input"]')
+        input_el.clear()
+        input_el.send_keys(str(interlocutor_id))
+        cls.driver.find_element(By.XPATH, '//button[@id="js-search-button"]').click()
+        sleep(0.5)  # wait for switch and history loading
 
+        try:
+            cls._find_cur_chat_el()
+        except NoSuchElementException:
+            pass
 
-def _test_positive_send_messages_in_cur_chat(text_messages: list[str]) -> None:
-    chat_input_container_el = _get_cur_chat_el().find_element(By.XPATH, 'div[@class="chat__input-container"]')
-    input_el = chat_input_container_el.find_element(By.XPATH, './/textarea')
-    button_el = chat_input_container_el.find_element(By.XPATH, './/button')
+    # FixMe: Вернуть прогон теста после того, как сделаем редактирование имени и фамилии
+    @classmethod
+    def _switch_to_chat_by_sidebar(cls, interlocutor_full_name: str) -> None:
+        cls.driver.find_element(By.XPATH, f'//div[@class="sidebar__links"]'
+                                          f'/div[@class="chat-link"]'
+                                          f'//div[@class="chat-link__chat-name"]'
+                                          f'[text()="{interlocutor_full_name}"]').click()
+        sleep(0.5)  # wait for switch and history loading
+        cls._find_cur_chat_el()
 
-    for text_message in text_messages:
+    @classmethod
+    def _find_cur_chat_el(cls) -> None:
+        cls.cur_chat_el = cls.driver.find_element(By.XPATH, '//div[@class="chat" and not(@class="chat--hidden")]')
+
+    @classmethod
+    def _send_message_in_cur_chat(cls, text_message: str) -> None:
+        chat_input_container_el = cls.cur_chat_el.find_element(By.XPATH, 'div[@class="chat__input-container"]')
+        input_el = chat_input_container_el.find_element(By.XPATH, './/textarea')
+        button_el = chat_input_container_el.find_element(By.XPATH, './/button')
+
         input_el.send_keys(text_message)
         button_el.click()
         sleep(0.5)  # wait for api chat message creating
 
+    @classmethod
+    def _change_users(cls, first_user: UserInfo,
+                      second_user: UserInfo,
+                      ) -> None:
+        old_user = cls.cur_user
+        new_user: UserInfo = first_user
+        if old_user == first_user:
+            new_user = second_user
+        cls._close_cur_chat()
+        cls._login(new_user)
+        cls._search_interlocutor(old_user.id)
 
-def _test_positive_history(expected_text_messages: list[str]) -> None:
-    text_messages_els = _get_cur_chat_el().find_elements(By.XPATH, './/div[@class="chat__message__text"]')
-    for i, text_message_el in enumerate(text_messages_els):
-        assert text_message_el.text == expected_text_messages[i]
+    @classmethod
+    def _close_cur_chat(cls) -> None:
+        cls.cur_chat_el.find_element(By.XPATH, './/div[@class="chat__back-link"]').click()
 
-
-def _test_positive_close_cur_chat() -> None:
-    _get_cur_chat_el().find_element(By.XPATH, './/div[@class="chat__back-link"]').click()
-
-
-def _get_cur_chat_el():
-    return driver.find_element(By.XPATH, '//div[@class="chat" and not(@class="chat--hidden")]')
+    @classmethod
+    def _test_positive_history(cls, expected_history: list[str]) -> None:
+        text_messages_els = cls.cur_chat_el.find_elements(By.XPATH, './/div[@class="chat__message__text"]')
+        for i, text_message_el in enumerate(text_messages_els):
+            assert text_message_el.text == expected_history[i]
