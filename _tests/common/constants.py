eafa7ff442b9c5e05373bee6f0606215b1fd99aa
danil <danil.shevelev.2004@mail.ru>
2024-04-29 22:08:13 +0500

    new constant `EMAIL_CODE`

--- _tests/common/constants.py
+++ _tests/common/constants.py
@@ -7,10 +7,12 @@ from web.endpoints import Url
 __all__ = (
     'WEBSITE_URL',
     'FullUrl',
+    'EMAIL_CODE',
 )
 
 host: str = HOST.replace('0.0.0.0', 'localhost')
 WEBSITE_URL: Final[str] = f'http://{host}:{PORT}'
+EMAIL_CODE: Final[int] = 9999
 
 
 class FullUrl(StrEnum):
