f43f1c62d1a5add057595d6325bc1db08463c633
danil <danil.shevelev.2004@mail.ru>
2024-05-10 11:46:55 +0500

    [fix]

--- static/js/main/_messaging/userInfo.js
+++ static/js/main/_messaging/userInfo.js
@@ -1,11 +1,11 @@
 const userNameEl = document.getElementById("js-user-name");
 const userIdParentEl = document.getElementById("js-user-id-parent");
-const userIdEl = document.getElementById("js-user-id");
-
-userIdParentEl.onclick = () => {
+userIdParentEl.onclick = (e) => {
     navigator.clipboard.writeText(userIdEl.textContent);
     notify("ID успешно скопирован!");
 }
+
+const userIdEl = document.getElementById("js-user-id");
 
 export function updateUserInfo(user) {
     userNameEl.textContent = user.firstName + " " + user.lastName;
