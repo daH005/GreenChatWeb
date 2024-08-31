fda2f921cd8db5916e6d7bf7e61b50147de422a1
dah005 <danil.shevelev.2004@mail.ru>
2024-08-31 19:18:08 +0500

    [#25]

--- static/js/main/background.js
+++ static/js/main/background.js
@@ -4,6 +4,7 @@ export async function updateBackground() {
     let backgroundFile = await requestUserBackground();
     let backgroundFileUrl = URL.createObjectURL(backgroundFile);
     updateBackgroundUrl(backgroundFileUrl);
+    backgroundEl.dispatchEvent(new Event("load")); // because default `load` call before loading of the `background` style
 }
 export function updateBackgroundUrl(url) {
     backgroundEl.style.background = `url(${url})`;
--- ts/main/background.ts
+++ ts/main/background.ts
@@ -6,6 +6,8 @@ export async function updateBackground(): Promise<void> {
     let backgroundFile = await requestUserBackground();
     let backgroundFileUrl = URL.createObjectURL(backgroundFile);
     updateBackgroundUrl(backgroundFileUrl);
+
+    backgroundEl.dispatchEvent(new Event("load"));  // because default `load` call before loading of the `background` style
 }
 
 export function updateBackgroundUrl(url: string): void {
