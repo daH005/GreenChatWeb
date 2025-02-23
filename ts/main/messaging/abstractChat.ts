e6ef0f97571aa0ad34e8a5b97dcd41c97974a157
dan005 <danil.shevelev.2004@mail.ru>
2025-02-23 11:57:06 +0500

    [#32]

--- ts/main/messaging/abstractChat.ts
+++ ts/main/messaging/abstractChat.ts
@@ -272,7 +272,9 @@ export abstract class AbstractHTMLChat extends AbstractHTMLTemplatedElement {
         this._isOpened = true;
 
         if (!(this._firstOpeningWas)) {
+            this._textareaEl.disabled = true;
             await this._loadInitial();
+            this._textareaEl.disabled = false;
         }
         await this._read();
     }
